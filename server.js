10da10fec651a918fc1151460b1bf51c568ecfa1
shay_daniel <shay@isom.org>
2020-12-01 20:55:52 -0800

    dashboard not working on heroku?

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 //set up port for express
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 //set up express
 const app = express();
